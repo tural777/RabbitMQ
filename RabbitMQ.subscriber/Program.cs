a1c94d1b241a5f54c0da849c254842b2fc35ade7
Tural Novruzov <TuraliNovruzov@outlook.com>
2021-05-09 17:53:19 +0400

    Add project files.

---
+++ RabbitMQ.subscriber/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQ.subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Connection RabbitMQ
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://jgamoggd:B57PiXmZthSF6RB_MsuAmnm-tjwFQJ_U@baboon.rmq.cloudamqp.com/jgamoggd");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            // olsa da olar, olmasa da
+            //channel.QueueDeclare("hello-queue", true, false, false);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            channel.BasicConsume("hello-queue", true, consumer);
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+                Console.WriteLine("Gelen Mesaj: " + message);
+            };
+
+
+
+            Console.ReadLine();
+        }
+      
+    }
+}
---
+++ RabbitMQ/Program.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Connection RabbitMQ
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://jgamoggd:B57PiXmZthSF6RB_MsuAmnm-tjwFQJ_U@baboon.rmq.cloudamqp.com/jgamoggd");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare("hello-queue", true, false, false);
+
+            string message = "hello world";
+
+            var messageBody = Encoding.UTF8.GetBytes(message);
+
+            // send a message to queue
+            // default exchange (the exchange name is the same as the queue name)
+            channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+
+            Console.WriteLine("Mesaj gonderilmishdir.");
+
+            Console.ReadLine();
+        }
+    }
+}
