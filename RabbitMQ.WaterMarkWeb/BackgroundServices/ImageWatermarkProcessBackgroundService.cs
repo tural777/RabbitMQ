2a9561bda50c09108bf30f0299030b18a558bcf8
Tural Novruzov <TuraliNovruzov@outlook.com>
2021-05-11 23:56:59 +0400

    add_ImageWatermarkProcessBackgroundService

---
+++ RabbitMQ.WaterMarkWeb/BackgroundServices/ImageWatermarkProcessBackgroundService.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.WaterMarkWeb.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.WaterMarkWeb.BackgroundServices
+{
+
+    public class ImageWatermarkProcessBackgroundService : BackgroundService
+    {
+        private readonly RabbitMQClientService _rabbitMQClientService;
+        private readonly ILogger<ImageWatermarkProcessBackgroundService> _logger;
+        private IModel _channel;
+
+        public ImageWatermarkProcessBackgroundService(RabbitMQClientService rabbitMQClientService, ILogger<ImageWatermarkProcessBackgroundService> logger)
+        {
+            _rabbitMQClientService = rabbitMQClientService;
+            _logger = logger;
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            _channel = _rabbitMQClientService.Connect();
+
+            _channel.BasicQos(0, 1, false);
+
+            return base.StartAsync(cancellationToken);
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var consumer = new AsyncEventingBasicConsumer(_channel);
+
+            _channel.BasicConsume(RabbitMQClientService.QueueName, false, consumer);
+
+            consumer.Received += Consumer_Received;
+
+            return Task.CompletedTask;
+        }
+
+
+        private Task Consumer_Received(object sender, BasicDeliverEventArgs @event)
+        {
+            var productImageCreatedEvent = JsonSerializer.Deserialize<productImageCreatedEvent>(
+                Encoding.UTF8.GetString(@event.Body.ToArray()));
+
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Images",
+                productImageCreatedEvent.ImageName);
+
+
+        }
+
+
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            return base.StopAsync(cancellationToken);
+        }
+    }
+}
--- RabbitMQ.WaterMarkWeb/Controllers/ProductsController.cs
+++ RabbitMQ.WaterMarkWeb/Controllers/ProductsController.cs
@@ -1,21 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RabbitMQ.WaterMarkWeb.Models;
+using RabbitMQ.WaterMarkWeb.Services;
 
 namespace RabbitMQ.WaterMarkWeb.Controllers
 {
     public class ProductsController : Controller
     {
         private readonly AppDbContext _context;
+        private readonly RabbitMQPublisher _rabbitMQPublisher;
 
-        public ProductsController(AppDbContext context)
+        public ProductsController(AppDbContext context, RabbitMQPublisher rabbitMQPublisher)
         {
             _context = context;
+            _rabbitMQPublisher = rabbitMQPublisher;
         }
 
         // GET: Products
@@ -53,14 +58,34 @@ namespace RabbitMQ.WaterMarkWeb.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Price,Stock,ImageName")] Product product)
+        public async Task<IActionResult> Create([Bind("Id,Name,Price,Stock,ImageName")]
+        Product product, IFormFile ImageFile)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid) return View(product);
+
+            if (ImageFile is { Length: > 0 })
             {
-                _context.Add(product);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                var randomImageName = Guid.NewGuid() + Path.GetExtension(ImageFile.FileName);
+
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images",
+                    randomImageName);
+
+                // C# 9.0
+                await using FileStream stream = new(path, FileMode.Create);
+
+                await ImageFile.CopyToAsync(stream);
+
+                _rabbitMQPublisher.Publish(new productImageCreatedEvent()
+                { ImageName = randomImageName });
+
+                product.ImageName = randomImageName;
             }
+
+
+
+            _context.Add(product);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
             return View(product);
         }
 
